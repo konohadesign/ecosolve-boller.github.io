d7aba7e104ece785e31f4214f8cca8e5f9973d13
Konoha Design <info@konoha-design.com>
2025-04-14 21:36:22 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import { Header } from "./components/Header";
 import Hero from "./components/Hero";
 import TrustSection from "./components/TrustSection";
@@ -12,24 +16,33 @@ import ScrollToTop from "./components/ScrollToTop";
 import { FAQ } from "./components/FAQ";
 import ROICalculator from './components/ROICalculator';
 
-export default function App() {
+function App() {
   return (
-    <div className="font-sans text-gray-900 bg-gradient-to-b from-white via-blue-50 to-white min-h-screen w-full">
-      <Header />
-      <main className="pt-24">
-        <Hero />
-        <TrustSection />
-        <ProblemSolution />
-        <ServiceCards />
-        <Technology />
-        <ROICalculator />
-        <CaseStudies />
-        <AboutShort />
-        <FAQ />
-        <ContactCTA />
-      </main>
-      <Footer />
-      <ScrollToTop />
-    </div>
+    <Router basename="/ecosolve-boller">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <div className="font-sans text-gray-900 bg-gradient-to-b from-white via-blue-50 to-white min-h-screen w-full">
+        <Header />
+        <main className="pt-24">
+          <Hero />
+          <TrustSection />
+          <ProblemSolution />
+          <ServiceCards />
+          <Technology />
+          <ROICalculator />
+          <CaseStudies />
+          <AboutShort />
+          <FAQ />
+          <ContactCTA />
+        </main>
+        <Footer />
+        <ScrollToTop />
+      </div>
+    </Router>
   );
 }
+
+export default App;
